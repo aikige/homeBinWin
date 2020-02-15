f9a0a3985a0eec62a37a6591f1199c9f039e6f5c
Aikige <48190566+aikige@users.noreply.github.com>
2020-02-15 13:31:41 +0900

    [up] add argument handling. and removed incorrect exe file.

--- dumpForegroundWindow.py
+++ dumpForegroundWindow.py
@@ -14,23 +14,47 @@ def format_date(date):
 def get_log_filename(date):
     return date + "-Window_Log.md"
 
-def log_active_window(interval):
+def log_active_window(interval, skip_duplicate):
+    """
+    check foreground window title and dump it into file.
+    output filename is generated from date.
+
+    Parameters
+    ----------
+    interval : int
+        interval of logging, in seconds.
+    skip_duplicate : boolean
+        select if duplicated title should be logged or not.
+        if this value is True, same title is not logged.
+    """
     day = format_date(datetime.datetime.today())
     old = ""
     with open(get_log_filename(day), "a", encoding="UTF-8", errors="ignore") as f:
         while day == format_date(datetime.datetime.now()):
             title = get_active_window_title()
-            if (title != old):
-                out = get_log_string(title)
-                print(out)
-                f.write(out + "\n")
-                f.flush()
-                time.sleep(interval)
-                old = title
+            if (skip_duplicate == True and title == old):
+                continue
+            out = get_log_string(title)
+            print(out)
+            f.write(out + "\n")
+            f.flush()
+            time.sleep(interval)
+            old = title
 
-def keep_logging(interval):
+def keep_logging(interval, skip_duplicate):
     while True:
-        log_active_window(interval)
+        log_active_window(interval, skip_duplicate)
 
 if __name__ == '__main__':
-    keep_logging(60)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--interval',
+            help='set interval of logging in seconds. default interval is 120',
+            type=int,
+            default=120)
+    parser.add_argument('-s', '--skip_duplicate',
+            help='if this argument is set, skip logging of duplicated title',
+            action='store_true',
+            default=False)
+    args = parser.parse_args()
+    keep_logging(args.interval, args.skip_duplicate);
