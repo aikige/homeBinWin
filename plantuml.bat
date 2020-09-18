7f5d216974892f508f9b5205f44c1b48c1ba8bb6
Aikige <48190566+aikige@users.noreply.github.com>
2020-09-18 11:43:40 +0900

    [up] Changed assumed environment - user is using AdoptOpenJDK or something similar and batch file is not required to add path to java.exe.

--- plantuml.bat
+++ plantuml.bat
@@ -1,10 +1,13 @@
 @echo off
 setlocal
-path C:\opt\jdk\bin;%PATH%
-set JAVA_HOME=c:\opt\jdk
+rem path C:\opt\jdk\bin;%PATH%
+rem set JAVA_HOME=c:\opt\jdk
 call :set_plantuml plantuml.jar
 
-if x%PLANTUML% == x exit /b
+if x%PLANTUML% == x (
+	echo No plantuml.jar found.
+	exit /b
+)
 if x%~nx1 == x (
 	start javaw.exe -jar %PLANTUML% -charset UTF-8 -gui
 ) else (
