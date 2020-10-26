c1e99038045461dbc0702f83480e9d5c7df6b927
Aikige <48190566+aikige@users.noreply.github.com>
2020-10-27 08:52:33 +0900

    [up] add portrait format.

--- md2pdf.bat
+++ md2pdf.bat
@@ -2,11 +2,31 @@
 REM Convert markdown to PDF using marp.exe
 REM https://github.com/marp-team/marp-cli
 REM Assumption: following files are placed on the directory which includes this script.
-REM - marp.css: style sheet.
+REM - marp.css / marp-doc.css: style sheet.
 REM - Background.png: background image.
 setlocal
 set BG_IMAGE=Background.png
-set BG_DIR=%~dp0
-if exist %BG_DIR%\%BG_IMAGE% if not exist Background.png mklink /h %BG_IMAGE% %BG_DIR%\%BG_IMAGE% 
-marp --allow-local-files --theme %USERPROFILE%\bin\marp.css --pdf %1
+set WORK_DIR=%~dp0
+
+set MDFILE=%1
+shift
+
+REM select layout
+if x%1 == x-doc (
+	set CSS=marp-doc.css
+	shift
+) else (
+	set CSS=marp.css
+)
+
+REM watch option
+if x%1 == x-w (
+	set WATCH=-w
+	shift
+)
+
+if not exist %CSS% if exist %WORK_DIR%\%CSS% set CSS=%WORK_DIR%\%CSS%
+if not exist %BG_IMAGE% if exist %WORK_DIR%\%BG_IMAGE% mklink /h %BG_IMAGE% %WORK_DIR%\%BG_IMAGE% 
+
+marp --allow-local-files %WATCH% --theme %CSS% --pdf %MDFILE%
 endlocal
