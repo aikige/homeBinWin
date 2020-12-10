95535ea179a61aac0d8457b6c32f22822a538e27
Aikige <48190566+aikige@users.noreply.github.com>
2020-12-10 23:38:54 +0900

    [up] add reference to API.

--- addUserPath.ps1
+++ addUserPath.ps1
@@ -2,6 +2,11 @@ $NewPath = Resolve-Path($args[0])
 echo "== New Item =="
 [string]$NewPath
 
+<#
+Reference:
+https://docs.microsoft.com/ja-jp/dotnet/api/system.environment.getenvironmentvariable
+https://docs.microsoft.com/ja-jp/dotnet/api/system.environment.setenvironmentvariable
+#>
 $PathStr = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
 $PathArray = $PathStr.Split(";")
 $PathStr = [System.Environment]::GetEnvironmentVariable("PATH", "User")
