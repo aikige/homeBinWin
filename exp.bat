1d6341c3b0af8e27b77f2b1bf5f877bac6eeaca5
Aikige <48190566+aikige@users.noreply.github.com>
2023-11-14 09:34:34 +0900

    [up] exp.bat calls Tablacus Explorer if it exists.

--- exp.bat
+++ exp.bat
@@ -1,1 +1,13 @@
-@explorer %CD%
+@echo off
+setlocal
+if "%PROCESSOR_ARCHITECTURE%" equ "x86" (
+    set TARGET=%USERPROFILE%\Apps\tablacus\TE32.exe
+) else (
+    set TARGET=%USERPROFILE%\Apps\tablacus\TE64.exe
+)
+if exist %TARGET% (
+    start %TARGET% %CD%
+) else (
+    @explorer %CD%
+)
+endlocal
