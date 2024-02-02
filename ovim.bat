10c3fa7b241e2636d01314b5eae4dcd2beca5d22
Aikige <48190566+aikige@users.noreply.github.com>
2024-02-02 11:09:15 +0900

    [up] avoid unexpected window closure when ovim is invoked from cmd.exe.

--- ovim.bat
+++ ovim.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -WindowStyle Hidden -file "%~dp0\sendToVim.ps1" %*
+powershell "%~dp0\sendToVim.ps1" %*
 REM Reference: https://arimasou16.com/blog/2018/12/06/00281/
 REM Reference: https://letspowershell.blogspot.com/2016/03/powershell.html
 rem "%~dp0\sendToVim.js" %*
