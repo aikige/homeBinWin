ece81082cc4f5462550aa0104938e0576c83e8b2
Aikige <48190566+aikige@users.noreply.github.com>
2024-02-13 12:14:49 +0900

    [up] not important: add comment about not working case.

--- ovim.bat
+++ ovim.bat
@@ -1,5 +1,7 @@
 @echo off
 powershell "%~dp0\sendToVim.ps1" %*
+REM Following style closes Command Prompt and not suitable for the purpose.
+rem powershell -WindowStyle Hidden "%~dp0\sendToVim.ps1" %*
 REM Reference: https://arimasou16.com/blog/2018/12/06/00281/
 REM Reference: https://letspowershell.blogspot.com/2016/03/powershell.html
 rem "%~dp0\sendToVim.js" %*
