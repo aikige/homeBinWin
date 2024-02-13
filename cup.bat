f4c5d62524aefb43f4fcc85c63e8ce343e34905a
Aikige <48190566+aikige@users.noreply.github.com>
2024-02-13 22:05:17 +0900

    [up] add special handling for vim.

--- cup.bat
+++ cup.bat
@@ -2,7 +2,12 @@
 if "%~1" == "" goto check_upgradable
 
 :exec_upgrade
+if "%1" == "vim" goto update_vim
 choco upgrade -y %*
+goto :eof
+
+:update_vim
+choco upgrade -y vim --params "/RestartExplorer"
 goto :eof
 
 :check_upgradable
