b00e3bde8ebc327799d3f3938262a78f5c8babde
Aikige <48190566+aikige@users.noreply.github.com>
2024-02-14 12:18:15 +0900

    [up] updated to support environment where coreutils exist.

--- ls.bat
+++ ls.bat
@@ -1,2 +1,12 @@
-@echo NOTE: replace "ls" with "dir"
+@echo off
+where ls.exe > nul
+if %ERRORLEVEL% == 0 goto ls
+
+:dir
+echo NOTE: replace "ls" with "dir"
 @dir %*
+goto :eof
+
+:ls
+ls.exe %*
+
--- rm.bat
+++ rm.bat
@@ -1,16 +1,30 @@
 @echo off
 setlocal
+where rm.exe > nul
+if %ERRORLEVEL% == 0 goto rm
+
 call :find bash.exe
-if x%BASH%==x (
-    del %*
-) else (
-    echo execute "rm %*" on %BASH%
-    %BASH% -c "rm %*"
-)
+if x%BASH%==x goto del
+
+:bash
+echo execute "rm %*" on %BASH%
+%BASH% -c "rm %*"
 endlocal
-goto :eof
+goto end
+
+:del
+echo execute "del %*"
+del %*
+goto end
+
+:rm
+rm.exe %*
+goto end
 
 :find
-set BASHPATH=%ProgramFiles%\git\bin;%ProgramFiles%\git\usr\bin
+set BASHPATH=%ProgramFiles%\git\bin;%ProgramFiles%\git\usr\bin;%PATH%
 set BASH=%~s$BASHPATH:1
 exit /b
+
+:end
+endlocal
