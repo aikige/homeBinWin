88ee0bfaa1955112a9804aba46a5ee14be366285
Aikige <48190566+aikige@users.noreply.github.com>
2024-02-27 09:55:51 +0900

    [up] cleanup.

--- findDllUsers.ps1
+++ findDllUsers.ps1
@@ -1,16 +1,16 @@
-# Reference: https://stackoverflow.com/questions/54310513/scripted-method-to-find-and-kill-process-using-a-specific-dll
 if ($args.Length -eq 0) {
-	echo "Argument is required"
+	echo "Argument is required, please provide search string(s)."
 	exit
 }
 echo "Check process uses $args"
+
+# Reference: https://stackoverflow.com/questions/54310513/scripted-method-to-find-and-kill-process-using-a-specific-dll
 foreach ($p in Get-Process)
 {
-	#echo "Checking process $p..."
 	foreach ($m in $p.modules)
 	{
 		foreach ($arg in $args) {
-			if ( $m.FileName -match $arg)
+			if ($m.FileName -match $arg)
 			{
 				write-host "Found:" $m.FileName "in" $p.Name "ID:" $p.id
 			}
