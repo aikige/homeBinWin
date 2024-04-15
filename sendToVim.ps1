47cc81a2a19f181e8154643fe08f54d3be865cd2
Aikige <48190566+aikige@users.noreply.github.com>
2024-04-15 12:06:15 +0900

    [up] simplified logic.

--- sendToVim.ps1
+++ sendToVim.ps1
@@ -1,20 +1,26 @@
-$scvim = $Env:USERPROFILE + '\scoop\apps\vim\current\gvim.exe'
 $dirs = @()
-$dirs += 'C:\opt\vim'
+# Scoop
+$dirs += $Env:USERPROFILE + '\scoop\apps\vim\current'
+# Chocolatey
 $dirs += 'C:\tools\vim'
+# My favorite location
+$dirs += 'C:\opt\vim'
+# Standard location
 $dirs += 'C:\Program Files\Vim'
 $dirs += 'C:\Program Files (x86)\Vim'
 $vimpath = 'C:\Windows'
 $ErrorActionPreference = "silentlycontinue"
-if (Test-Path $scvim) {
-	$vimpath = $Env:USERPROFILE + '\scoop\apps\vim\current'
-} else {
-	foreach ($dir in $dirs) {
-		$dirs = Get-ChildItem -Path $dir -Filter vim* | Sort-Object -Descending
-		if ($dirs.Length -gt 1) {
-			$vimpath = $dir + '\' + $dirs[0]
-			break
-		}
+foreach ($dir in $dirs) {
+	if (Test-Path $dir + '\gvim.exe') {
+		# The directory has gvim.exe inside.
+		$vimpath = $dir
+		break
+	}
+	$dirs = Get-ChildItem -Path $dir -Filter vim* | Sort-Object -Descending
+	if ($dirs.Length -gt 1) {
+		# The directory has vimXX subfolder.
+		$vimpath = $dir + '\' + $dirs[0]
+		break
 	}
 }
 $ErrorActionPreference = "continue"
