0d021c0806e55731058f28756e354d03c1e6eb8a
Aikige <48190566+aikige@users.noreply.github.com>
2024-06-10 13:03:06 +0900

    [up] use sub-routine instead of goto.

--- sup.bat
+++ sup.bat
@@ -1,11 +1,19 @@
 @echo off
-if "%~1" == "" goto check_upgradable
-
-:exec_upgrade
-scoop update %*
+if "%~1" == "" (
+    call :check_upgradable
+) else if "%~1" == "all" (
+    call :handle_upgrade *
+) else (
+    call :handle_upgrade %*
+)
 goto :eof
+
+:handle_upgrade
+call scoop update %*
+exit /b
 
 :check_upgradable
 call scoop update
 call scoop status -l
+exit /b
 
