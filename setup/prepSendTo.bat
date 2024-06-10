8a3c6b6fbacdd9f9cdee130b154e66ddbbe29a23
Aikige <48190566+aikige@users.noreply.github.com>
2024-06-10 14:50:47 +0900

    [up] add link to copyDataUrlToClipboard.

--- setup/prepSendTo.bat
+++ setup/prepSendTo.bat
@@ -1,6 +1,15 @@
 call :createPS1Link GVim.lnk ..\sendToVim.ps1 -a
+call :createLink copyDataUrlToClipboard.lnk ..\copyDataUrlToClipboard.pyw
 exit /b
 
 :createPS1Link
+REM 1st parameter: Link name
+REM 2nd parameter: Link target script path (relative/absolute)
 powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell -File "%~s2" %3 %4 %5
+exit /b
+
+:createLink
+REM 1st parameter: Link name
+REM 2nd parameter: Link target script path (relative/absolute)
+powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" "%~s2" %3 %4 %5
 exit /b
