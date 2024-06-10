dc8d4d53828bbdccb746a2a8a7cadb9d209da4bb
Aikige <48190566+aikige@users.noreply.github.com>
2024-06-10 14:35:23 +0900

    [up] use -File to specify powershell script and also use short name.

--- setup/prepSendTo.bat
+++ setup/prepSendTo.bat
@@ -2,5 +2,5 @@ call :createPS1Link GVim.lnk ..\sendToVim.ps1 -a
 exit /b
 
 :createPS1Link
-powershell -file ..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell "%~df2"
+powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell -File "%~s2" %3 %4 %5
 exit /b
