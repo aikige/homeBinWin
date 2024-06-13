c6cc8316e3675afc801458c388646331b3efe349
Aikige <48190566+aikige@users.noreply.github.com>
2024-06-13 16:59:16 +0900

    [up] safer statement.

--- setup/prepSendTo.bat
+++ setup/prepSendTo.bat
@@ -1,15 +1,15 @@
-call :createPS1Link GVim.lnk ..\sendToVim.ps1 -a
-call :createLink copyDataUrlToClipboard.lnk ..\copyDataUrlToClipboard.pyw
+call :createPS1Link GVim.lnk "%~dp0\..\sendToVim.ps1"
+call :createLink copyDataUrlToClipboard.lnk "%~dp0\..\copyDataUrlToClipboard.pyw"
 exit /b
 
 :createPS1Link
 REM 1st parameter: Link name
 REM 2nd parameter: Link target script path (relative/absolute)
-powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell -File "%~s2" %3 %4 %5
+powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell.exe -File "%~f2" %3 %4 %5
 exit /b
 
 :createLink
 REM 1st parameter: Link name
 REM 2nd parameter: Link target script path (relative/absolute)
-powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" "%~s2" %3 %4 %5
+powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" "%~f2" %3 %4 %5
 exit /b
