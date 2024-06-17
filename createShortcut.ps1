1dc0fb55156afd5591f77f86efb897eef86a382d
Aikige <48190566+aikige@users.noreply.github.com>
2024-06-17 10:00:46 +0900

    [up] cleanup shortcut script.

--- createShortcut.ps1
+++ createShortcut.ps1
@@ -1,18 +1,44 @@
-if ($args.Length -lt 2) {
-	echo "Need 2 arguments: LINK_NAME TARGET_PROGRAM ARGS"
-	exit
-}
-if ($args[0] -eq '-m') {
+<#
+.SYNOPSIS
+createShortcut.ps1 [-Minimized] <LinkName> <TargetProgram> [<Args> ...]
+
+.DESCRIPTION
+Creates a link for the specified program.
+
+.PARAMETER Minimized
+A switch to minimize the Window invoked from the shortcut.
+This switch shall be placed as 1st argument.
+.PARAMETER LinkName
+1st positional parameter that specifies name of the link.
+.PARAMETER TargetProgram
+2nd positional parameter that specifies target file to be linked.
+.PARAMETER Args
+List of arguments that is passed to shortcut as Arguments.
+Multiple arguments can be specified.
+
+.NOTE
+This script does not follow standard parameter manner in PowerShell and
+`Get-Help` for this script will not work as expected.
+#>
+if (($args.Length -gt 0) -and ($args[0] -eq '-Minimized')) {
 	$minimize = $true
 	$null, $args = $args	# Shift args.
 } else {
 	$minimize = $false
 }
+if ($args.Length -lt 2) {
+	echo "Need 2 arguments: [-Minimized] <LinkName> <TargetProgram> [<Args> ...]"
+	exit
+}
+$link_name = $args[0]
+$target_program = $args[1]
+$null, $null, $args = $args
+echo $link_name
+echo $target_program
 $ws =  New-Object -ComObject WScript.Shell
-$sc = $ws.CreateShortcut($args[0])
-$sc.TargetPath = $args[1]
-if ($args.Length -gt 2) {
-	$null, $null, $args = $args
+$sc = $ws.CreateShortcut($link_name)
+$sc.TargetPath = $target_program
+if ($args.Length -gt 0) {
 	$sc.Arguments = "$args"
 }
 if ($minimize) {
--- setup/prepSendTo.bat
+++ setup/prepSendTo.bat
@@ -5,11 +5,11 @@ exit /b
 :createPS1Link
 REM 1st parameter: Link name
 REM 2nd parameter: Link target script path (relative/absolute)
-powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell.exe -File "%~f2" %3 %4 %5
+powershell -File %~dp0\..\createShortcut.ps1 -Minimized "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" powershell.exe -File "%~f2" %3 %4 %5
 exit /b
 
 :createLink
 REM 1st parameter: Link name
 REM 2nd parameter: Link target script path (relative/absolute)
-powershell -File %~dp0\..\createShortcut.ps1 -m "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" "%~f2" %3 %4 %5
+powershell -File %~dp0\..\createShortcut.ps1 -Minimized "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\%1" "%~f2" %3 %4 %5
 exit /b
