7e99a986b3d2f64eb57a5fdfeeaf7322f041697d
Aikige <48190566+aikige@users.noreply.github.com>
2025-12-08 09:16:12 +0900

    [up] use argparse feature to create list of pages.

--- extPdf.py
+++ extPdf.py
@@ -11,9 +11,18 @@ def extract_pages(input_pdf_path, output_pdf_path, pages_to_extract):
     """
     reader = PdfReader(input_pdf_path)
     writer = PdfWriter()
+    pages = []  # List of pages, integer type.
+
+    # Create list of pages.
+    for p in pages_to_extract:
+        if '-' in p:
+            [start, end] = map(int, p.split('-'))
+            pages += list(range(start, end + 1))
+        else:
+            pages.append(int(p))
 
     # Adjust page number since array `pages` is zero-based indexing.
-    for page_num in pages_to_extract:
+    for page_num in pages:
         if 1 <= page_num <= len(reader.pages):
             writer.add_page(reader.pages[page_num - 1])
         else:
@@ -22,14 +31,13 @@ def extract_pages(input_pdf_path, output_pdf_path, pages_to_extract):
     with open(output_pdf_path, "wb") as output_pdf:
         writer.write(output_pdf)
 
-    print(f"Extracted {len(pages_to_extract)} pages, and stored to {output_pdf_path}.")
+    print(f"Extracted {len(pages)} pages, and stored to {output_pdf_path}.")
 
 if __name__=="__main__":
     import argparse
     parser = argparse.ArgumentParser(description='A simple program which creates PDF subset')
     parser.add_argument('input_file', help='The input file to be cropped')
     parser.add_argument('output_file', help='The output file which stores cropped PDF')
-    parser.add_argument('pages_to_extract', help='comma separated number list of pages')
+    parser.add_argument('page', nargs='+', help='page (e.g. 2) or range (e.g. 1-5) of pages to be extracted')
     args = parser.parse_args()
-    pages = map(int,args.pages_to_extract.split(','))
-    extract_pages(args.input_file, args.output_file, pages)
+    extract_pages(args.input_file, args.output_file, args.page)
