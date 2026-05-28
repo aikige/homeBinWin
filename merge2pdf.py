45167013a77e95590d5704e92b993dec5f73cd50
Aikige <48190566+aikige@users.noreply.github.com>
2026-05-28 23:05:09 +0900

    [up] cleanup.

--- merge2pdf.py
+++ merge2pdf.py
@@ -4,50 +4,42 @@ from pypdf import PdfReader, PdfWriter
 import os
 import tempfile
 
+def append_pdf(filename, writer):
+    with open(filename, "rb") as f:
+        writer.append(f)
+
 def merge_to_single_pdf(input_files, output_pdf):
     writer = PdfWriter()
-
     for file in input_files:
         ext = os.path.splitext(file)[1].lower()
-
-        # --- 画像ファイルの場合 ---
+        # --- image files ---
         if ext in [".jpg", ".jpeg", ".png"]:
             img = Image.open(file)
             if img.mode != "RGB":
                 img = img.convert("RGB")
-
-            # 一時PDFとして保存し、それを処理
+            # save as temporal PDF and process it.
             with tempfile.NamedTemporaryFile(delete_on_close=False, suffix=".pdf") as fp:
                 img.save(fp.name)
-                # PDFとして読み込み
-                with open(fp.name, "rb") as f:
-                    reader = PdfReader(f)
-                    for page in reader.pages:
-                        writer.add_page(page)
-            
-        # --- PDFファイルの場合 ---
+                # append the temporal file to output.
+                append_pdf(fp.name, writer)
+        # --- PDF files ---
         elif ext == ".pdf":
-            with open(file, "rb") as f:
-                reader = PdfReader(f)
-                for page in reader.pages:
-                    writer.add_page(page)
-
+            append_pdf(file, writer)
         else:
-            print(f"未対応ファイル形式: {file}")
-
-    # 出力
+            print(f"Unknown file type: {file}")
+    # At the end, write to the file.
     with open(output_pdf, "wb") as f:
         writer.write(f)
-
     print(f"merged: {output_pdf}")
 
 if __name__=="__main__":
     import argparse
+    default_output = f"{os.path.expanduser("~")}{os.sep}Downloads{os.sep}output.pdf"
     parser = argparse.ArgumentParser(
             description='Convert a list of files into a single PDF')
     parser.add_argument('file', nargs='+',
             help='input file(s) for PDF conversion (JPEG, PNG or PDF)')
-    parser.add_argument('-o', '--output', default='output.pdf',
-            help='the output file. default value is "output.pdf"')
+    parser.add_argument('-o', '--output', default=default_output,
+            help=f"the output file. default value is '{default_output}'")
     args = parser.parse_args()
-    merge_to_single_pdf(args.file, args.output)
+    merge_to_single_pdf(sorted(args.file), args.output)
